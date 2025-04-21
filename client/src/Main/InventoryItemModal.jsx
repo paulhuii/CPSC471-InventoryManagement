93f0a5c5e75243d41e316c3777796a3a5ed10239
annaisjoy <annale3898@gmail.com>
2025-04-21 10:20:36 -0600

    clean up

--- client/src/Main/InventoryItemModal.jsx
+++ client/src/Main/InventoryItemModal.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 
 function InventoryItemModal({
   item,       
+  onClose,
   onSubmit,   // Function to call with submitted data
   initialError,
 }) {
